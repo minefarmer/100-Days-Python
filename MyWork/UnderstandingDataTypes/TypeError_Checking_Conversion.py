c0ef880224c5fa5fe87432d2fb4911b8990d6a25
minefarmer <pgoldfarm@yahoo.com>
2021-12-12 22:03:16 -0600

    bye^x

--- MyWork/UnderstandingDataTypes/TypeError_Checking_Conversion.py
+++ MyWork/UnderstandingDataTypes/TypeError_Checking_Conversion.py
@@ -12,7 +12,42 @@
                             # TypeError: can only concatenate str (not "int") to str
 
 
-num_char = len(input("What is your name?"))  # Rich
-# print("Your name has " + num_char + " characters.")
+# num_char = len(input("What is your name?"))  # Rich
+# # print("Your name has " + num_char + " characters.")
 
-print(type(num_char))  # <class 'int'>
+# print(type(num_char))  # <class 'int'>
+
+
+# num_char = len(input("What is your name? "))  # Rich
+# # print("Your name has " + num_char + " characters.")
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")  # Your name has 4 characters.
+
+
+# a = 123
+# print(type(a))  # <class 'int'>
+
+
+# a = str(123)
+# print(type(a))  # <class 'str'>
+
+
+# a = float(123)
+# print(type(a))  # <class 'float'>
+
+# print(70 + float("100.5"))  # 170.5
+
+
+# a = 123
+# print(type(a))
+
+# print(str(70) + str(100))
+
+
+# len(4837)  # Traceback (most recent call last):
+            # File "/home/rich/Desktop/CarlsHub/100-Days-Python/MyWork/UnderstandingDataTypes/TypeError_Checking_Conversion.py", line 49, in <module>
+            #     len(4837)
+            # TypeError: object of type 'int' has no len()
+
