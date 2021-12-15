e796e390f2da1a83a8d6ef66377c693df066c2d7
minefarmer <pgoldfarm@yahoo.com>
2021-12-14 20:49:07 -0600

    MyWork: Good Night

---
+++ MyWork/Randomization_PythonLists/RandomModule/main.py
@@ -0,0 +1,17 @@
+import random
+import my_module  # How to import a module
+
+random_integer = random.randint(1, 10)
+print(random_integer)  # 8
+
+random_float = random.random()
+print(random_float)  # 0.5788825473183001
+
+random_float = random.random()
+print(random_float)
+
+randomFloat = random.random() * 5
+print(randomFloat)  # 2.9423428400910105
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")  # Your love score is 71
