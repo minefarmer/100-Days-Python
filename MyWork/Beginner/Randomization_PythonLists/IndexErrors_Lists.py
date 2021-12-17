a5c1f2bb13507ff8095c19ef5beb72a8db3a4cad
minefarmer <pgoldfarm@yahoo.com>
2021-12-17 15:02:41 -0600

    switching to linux

--- MyWork/Beginner/Randomization_PythonLists/IndexErrors_Lists.py
+++ MyWork/Beginner/Randomization_PythonLists/IndexErrors_Lists.py
@@ -29,4 +29,11 @@
 
 # dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peachs", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
 
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)  # [['Strawberries', 'Nectarines', 'Apples', 'Grapes', 'Peaches', 'Cherries', 'Pears'], ['Spinach', 'Kale', 'Tomatoes', 'Celery', 'Potatoes']]
+
 
