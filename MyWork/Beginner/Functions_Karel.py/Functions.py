76e514d737e31ab0522189b1b0ad2d01d6c9221d
minefarmer <pgoldfarm@yahoo.com>
2021-12-17 20:57:25 -0600

    bye for now

---
+++ MyWork/Beginner/Functions_Karel.py/Functions.py
@@ -0,0 +1,19 @@
+'''     Defining functions
+def my_functiom():
+    # do this
+    # then do this
+    # finally do this
+
+'''
+
+
+print("Hello")  ## Hello     # print is a function
+num_char = len("Hello")   # num_char is a variable
+print(num_char)  # Hello
+
+def my_function():   # def is a keyword
+    print("HI!")
+    print("Bye")
+
+my_function() # HI!  ## This calls and triggers the function 
+                # Bye
---
+++ MyWork/Beginner/Functions_Karel.py/While_Loops.py
@@ -0,0 +1,8 @@
+'''     While Loop
+
+while something is True:  # It keeps repeating until the condition is true
+    # do this
+    # then do this
+    # then do this
+
+'''
