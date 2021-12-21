472be0c22221f2859774978a6f23aa48eab1be22
minefarmer <pgoldfarm@yahoo.com>
2021-12-21 14:16:41 -0600

    Bye for now

---
+++ MyWork/Beginner/Dictionaries/DeepDive.py
@@ -0,0 +1,7 @@
+programming_dictionary {
+    "Bug": "An error ina programm that prevents the programm from running as expected.",
+    "Function": "A piece of code that you can easily call over over again.",
+    "Loop": "The action of doing something over and over again." 
+}
+
+list[2] 
