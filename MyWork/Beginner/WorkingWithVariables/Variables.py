3d87dd91e56c83cf79cc0ab62e9a6fc72e4a5f6e
minefarmer <pgoldfarm@yahoo.com>
2021-12-23 23:51:05 -0600

    Good Night from Linux

--- MyWork/Beginner/WorkingWithVariables/Variables.py
+++ MyWork/Beginner/WorkingWithVariables/Variables.py
@@ -4,6 +4,24 @@
 # name = "Angela"
 # print(name)  # Angela
 
-name = input("What is your name?")  # Rich
-length = len(name)
-print(length)  # 4
+# name = input("What is your name?")  # Rich
+# length = len(name)
+# print(length)  # 4
+
+
+# Coding exercise
+
+a = input("a: ")
+b = input("b: ")
+
+# my code
+
+c = a
+a = b
+b = c
+
+
+#  end
+
+print("a = " + a)
+print("b = " + b)
