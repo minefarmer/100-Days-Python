d8806ef9c848d6f15288b330da281f8212d05263
minefarmer <pgoldfarm@yahoo.com>
2021-12-25 23:33:37 -0600

    good night

---
+++ MyWork/Intermediate/ObjectOrientatedProgramming/ClassesObjects.py
@@ -0,0 +1,18 @@
+'''
+Class
+
+                             /is holding plate = True
+                            / 
+                attributes:/
+                /    \
+            /      tables_responsible = [4, 5, 6]
+        waiter  (object)
+            \
+                \           / def take_order(table, order)
+                        /   # takes order to chef
+                methods:/
+                    \
+                        \
+                        \def take_payment(amount):
+                            # add money to the restaurant
+'''
